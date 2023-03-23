126bf2b1c8b4e93fdef7ec14e061b1d3e946f376
Inky19 <f.marie260@gmail.com>
2023-03-23 18:53:46 +0100

    Refactored drawer and searchview

--- app/src/main/java/com/memable/MainActivity.java
+++ app/src/main/java/com/memable/MainActivity.java
@@ -3,29 +3,24 @@ package com.memable;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
-import android.database.sqlite.SQLiteDatabase;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.memable.databinding.ActivityMainBinding;
-import com.memable.databinding.ContentMainBinding;
 
 import java.io.ByteArrayOutputStream;
 
@@ -35,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private DrawerLayout drawer;
     private LinearLayout recents;
+    private TextView emptyRecent;
+
+    private SearchView searchView;
     public static final int IMAGE_RESPONSE = 3;
 
     @Override
@@ -46,8 +44,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         Toolbar toolbar = binding.appBarMain.toolbar;
         recents = binding.appBarMain.contentMain.recentImages;
+        emptyRecent = binding.appBarMain.contentMain.emptyRecent;
+        searchView = binding.appBarMain.searchView;
 
-
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.setIconified(false);
+            }
+        });
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.drawer_settings, R.id.drawer_profile, R.id.drawer_about)
                 .setOpenableLayout(drawer)
                 .build();
 
@@ -104,6 +109,12 @@ public class MainActivity extends AppCompatActivity {
             newImage.requestLayout();
         } else {
             System.out.println("NON");
+        }
+
+        if (recents.getChildCount() > 0){
+            emptyRecent.setVisibility(View.INVISIBLE);
+        } else {
+            emptyRecent.setVisibility(View.VISIBLE);
         }
 
     }
